daff11a737bdec2651f891a460e32492c2a6be8e
leohn <leonhudak@protonmail.com>
2023-07-22 22:45:54 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { ProposalForm } from './components/ProposalForm/ProposalForm';
+
+function App() {
+  return (
+    <>
+      <ProposalForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ProposalForm/ProposalForm.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import styles from './ProposalForm.module.css';
+import { getUnixTime } from 'date-fns';
+
+export function ProposalForm() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [deadline, setDeadline] = useState(0); // deadline for the proposal in unix seconds
+  const [dateTime, setDateTime] = useState(
+    new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString().slice(0, 16)
+  );
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDateTime(event.target.value);
+    console.log('Unix timestamp: ', getUnixTime(new Date(event.target.value)));
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <h1>Create a Proposal!</h1>
+      <label className={styles.label}>
+        <div>Proposal title:</div>
+        <input
+          type="text"
+          value={title}
+          placeholder="Title..."
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </label>
+      <label className={styles.label}>
+        <div>Proposal description:</div>
+        <textarea
+          value={description}
+          placeholder="Description..."
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+      </label>
+      <label className={styles.label}>
+        <div>Proposal deadline:</div>
+        <input type="datetime-local" value={dateTime} onChange={handleChange} />
+      </label>
+
+      <label className={styles.label}>
+        <button>Publish proposal</button>
+      </label>
+    </form>
+  );
+}
