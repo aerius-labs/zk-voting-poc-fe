f1e4133d1a99efa4921dcbbdaba174db796556a0
Rahul Ghangas <rahulghangas0@gmail.com>
2023-07-23 15:07:48 +0530

    fix: snarkyjs import and add submodule

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,22 @@ import './App.css';
 import { ProposalForm } from './components/ProposalForm/ProposalForm';
 import Navbar from './components/Navbar/Navbar';
 import { VotingForm } from './components/VotingForm/VotingForm';
+import {
+  isReady,
+} from 'snarkyjs';
+import { useEffect } from 'react';
 
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      await isReady;
+      const { UserProof, AggregatorProof } = await import('../zk-voting-poc/build/src');
+      if (UserProof === undefined || AggregatorProof === undefined) {
+        throw new Error('Could not load zk-voting-poc');
+      };
+      
+    })();
+  }, []);
   return (
     <Router>
       <Navbar />
